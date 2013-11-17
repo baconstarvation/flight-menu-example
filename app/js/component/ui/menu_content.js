4898feb019fb5fb83129bcde1b0a072029288777
baconstarvation <ant.ucd@gmail.com>
2013-11-16 16:00:30 -0800

    push 1

---
+++ app/js/component/ui/menu_content.js
@@ -0,0 +1,17 @@
+define(function (require) {
+	'use strict';
+	
+	var defineComponent = require('flight/lib/component');
+	
+	return defineComponent(menuContent);
+	
+	function menuContent() {
+		this.setText = function (e, data) {
+			this.$node.html(data.markup);
+		}
+		
+		this.after('initialize', function () {
+			this.on(document, 'dataMenuSection', this.setText);
+		});
+	}
+});
